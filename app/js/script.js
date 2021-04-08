c76e5490e7b33364248d88a37662eb7062a57459
Daria <zhuromskaya_ds@mail.ru>
2021-04-08 17:02:03 +0700

    feat: footer

--- app/js/script.js
+++ app/js/script.js
@@ -1,29 +1,27 @@
 const btnHumburger = document.querySelector("#btnHumburger");
 const body= document.querySelector('body');
 const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHumburger.addEventListener('click', () => {
-    
-    if(header.classList.contains('open')) {
-        body.classList.remove('noscroll'); 
-        header.classList.remove('open');           
-         fadeElems.forEach((elem) =>{
-            elem.classList.remove('fade-in');
-            elem.classList.add('fade-out');
-            
-        });    
-    } else {
-        body.classList.add('noscroll'); 
-        overlay.classList.remove('fade-out'); 
-        header.classList.add('open');        
-        fadeElems.forEach((elem) =>{
-            elem.classList.remove('fade-out'); 
-            elem.classList.add('fade-in');            
-        });
-    }
-    console.log(body);
+const openMenuAnimate = () => {
+    fadeElems.forEach((elem) =>{
+        elem.classList.remove('fade-in');
+        elem.classList.add('fade-out');               
+    });    
+};
 
-    
-})
+const closeMenuAnimate = () => {
+    fadeElems.forEach((elem) =>{
+        elem.classList.remove('fade-out'); 
+        elem.classList.add('fade-in');            
+    });
+};
+
+body.addEventListener('click', (event) => {
+    const target = event.target;    
+    if (target.closest('#btnHumburger') || target.closest('.overlay'))  {
+        header.classList.contains('open') ? openMenuAnimate() : closeMenuAnimate();
+        body.classList.toggle('noscroll'); 
+        header.classList.toggle('open');
+    }   
+});
